c9a07dfba08e1ad5f6e7517928d239667cb95051
Robson Natanael <natanaelrobson@gmail.com>
2024-01-21 13:17:09 -0300

    feat: theme dark added

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,19 +1,20 @@
-import { LogOut } from "lucide-react";
 import { Button } from '@app/components/Button'
+import { LogOut } from 'lucide-react'
 
 export function Profile() {
   return (
     <div className="grid grid-cols-profile items-center gap-3">
       <img
-        src="https://github.com/robsonnatanael.png" className="h-10 w-10 rounded-full"
+        src="https://github.com/diego3g.png"
+        className="h-10 w-10 rounded-full"
         alt=""
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
-          Robson Natanael
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Diego Fernandes
         </span>
-        <span className="truncate text-sm text-zinc-500">
-          dev@robsonnatanael.com.br
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
+          diego@rocketseat.com.br
         </span>
       </div>
       <Button type="button" variant="ghost">
